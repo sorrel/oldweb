8bfc9ded6659f987b1552b79d371dfc87a8e731b
Duncan Hurwood <duncan.hurwood@sky.com>
2010-02-15 21:29:08 +0000

    Passwords added

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,37 @@ class User < ActiveRecord::Base
     # Password validations.
     validates_presence_of :password
     validates_length_of   :password, :within => 6..20
+    
+    
+    # Password functions **********************************************
+    before_save :encrypt_password
+
+    # Return true if the user's password matches the submitted password.
+    def has_correct_password?(submitted_password)
+        encrypted_password == encrypt(submitted_password)
+    end
+
+    def self.authenticate(email, submitted_password)
+        user = find_by_email(email)
+        return nil  if user.nil?
+        return user if user.has_correct_password?(submitted_password)
+    end
+
+    private
+      def encrypt_password
+            self.salt = make_salt
+            self.encrypted_password = encrypt(password)
+      end
+
+      def encrypt(string)
+            secure_hash("#{salt}#{string}")
+      end
+
+      def make_salt
+            secure_hash("#{Time.now.utc}#{password}")
+      end
+
+      def secure_hash(string)
+            Digest::SHA2.hexdigest(string)
+      end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100214210948) do
+ActiveRecord::Schema.define(:version => 20100215205003) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(:version => 20100214210948) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
