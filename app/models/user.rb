f431e599cb6c658abbed7d53425281af0f66e513
Duncan Hurwood <duncan.hurwood@sky.com>
2010-02-15 20:24:14 +0000

    Yesterday's leftovers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 # == Schema Information
-# Schema version: 20100214171534
+# Schema version: 20100214210948
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
