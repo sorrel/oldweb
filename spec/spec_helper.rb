74cba4209fa537488e8eb031e9f7ecc8e97c7784
Duncan Hurwood <duncan.hurwood@sky.com>
2010-03-26 20:52:51 +0000

    First ex

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,6 +29,13 @@ Spec::Runner.configure do |config|
   def test_sign_in(user)
     controller.current_user = user
   end
+  
+  def integration_sign_in(user)
+    visit signin_path
+    fill_in :email,    :with => user.email
+    fill_in :password, :with => user.password
+    click_button
+  end
   # == Fixtures
   #
   # You can declare fixtures for each example_group like this:
